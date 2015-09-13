40c7cb6748b5e7e1acf95ca6e093ab24db71422f
anthonypc <anthony@contoleon.com>
2015-09-13 12:24:59 +1000

    Functional Simple Graph

--- ui.r
+++ ui.r
@@ -1,15 +1,13 @@
 library(shiny)
 library(ggplot2)
 
-dataset <- diamonds
+dataset <- na.omit(read.csv("https://www.dropbox.com/s/9dw6hanf0ce6x9l/201509-price.csv?dl=1", header = TRUE, sep=",", quote="\"", col.names = c("url","head_line","ad_copy","query","date","price01","price02","brand","price","price_log"), row.names = NULL))
 
 fluidPage(
+  titlePanel("Advertised Flight Prices"),
   
-  titlePanel("Diamonds Explorer"),
-  
-  sidebarPanel( 
-    selectInput('color', 'Color', c('None', dataset$brand)),
-  ),
+  sidebarPanel(
+    ),
   
   mainPanel(
     plotOutput('plot')
