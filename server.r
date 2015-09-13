30b22e737f6129760e9079b5ea26d36e5f849544
anthonypc <anthony@contoleon.com>
2015-09-13 12:49:52 +1000

    Now with a date section.

--- server.r
+++ server.r
@@ -1,12 +1,28 @@
 library(shiny)
 library(ggplot2)
+library(dplyr)
 
-function(input, output) {
-  dataset <- na.omit(read.csv("https://www.dropbox.com/s/9dw6hanf0ce6x9l/201509-price.csv?dl=1", header = TRUE, sep=",", quote="\"", col.names = c("url","head_line","ad_copy","query","date","price01","price02","brand","price","price_log"), row.names = NULL))
+flightSet <- na.omit(read.csv("https://www.dropbox.com/s/9dw6hanf0ce6x9l/201509-price.csv?dl=1", header = TRUE, sep=",", quote="\"", col.names = c("url","head_line","ad_copy","query","date","price01","price02","brand","price","price_log"), row.names = NULL))
+flightSet$date <- as.Date(flightSet$date)
+
+function(input, output){
+
+  dataset <- reactive({
+    mindate <- input$date[1]
+    maxdate <- input$date[2]
+    
+    f <- flightSet %>%
+      filter(
+        date >= mindate,
+        date <= maxdate
+      ) %>%
+      arrange(price)
+    
+    })
 
   output$plot <- renderPlot({
     
-    p <- ggplot(dataset, aes(x = date, y = price, colour = brand)) + geom_point()
+    p <- ggplot(dataset(), aes(x = date, y = price, colour = brand)) + geom_point()
     
     print(p)
     
--- ui.r
+++ ui.r
@@ -1,12 +1,17 @@
 library(shiny)
 library(ggplot2)
+library(dplyr)
 
 dataset <- na.omit(read.csv("https://www.dropbox.com/s/9dw6hanf0ce6x9l/201509-price.csv?dl=1", header = TRUE, sep=",", quote="\"", col.names = c("url","head_line","ad_copy","query","date","price01","price02","brand","price","price_log"), row.names = NULL))
+dataset$date <- as.Date(dataset$date)
 
 fluidPage(
   titlePanel("Advertised Flight Prices"),
   
   sidebarPanel(
+    dateRangeInput("date", "Date range:",
+                   start = "2014-03-10",
+                   end   = "2015-09-11")
     ),
   
   mainPanel(
